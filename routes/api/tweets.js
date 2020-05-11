3d7d97bcf3b5d6b1a7a2c2cd0b93914340c677f5
Isaac <iyoon22@gmail.com>
2020-05-10 17:31:31 -0700

    Add MERN_Twitter project

---
+++ routes/api/tweets.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Tweet = require('../../models/Tweet');
+const validateTweetInput = require('../../validation/tweets');
+
+
+// Retrieves all tweets
+router.get('/', (req, res) => {
+  Tweet.find()
+    .sort({ date: -1 })
+    .then(tweets => res.json(tweets))
+    .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
+});
+
+
+// Retrieves a single user's tweets
+router.get('/user/:user_id', (req, res) => {
+  Tweet.find({ user: req.params.user_id })
+    .then(tweets => res.json(tweets))
+    .catch(err =>
+      res.status(404).json({ notweetsfound: 'No tweets found from that user' }
+      )
+    );
+});
+
+
+// Retrieves a single tweet
+router.get('/:id', (req, res) => {
+  Tweet.findById(req.params.id)
+    .then(tweet => res.json(tweet))
+    .catch(err =>
+      res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+    );
+});
+
+// Protected Route for a user to post tweets
+router.post('/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateTweetInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newTweet = new Tweet({
+      text: req.body.text,
+      user: req.user.id
+    });
+
+    newTweet.save().then(tweet => res.json(tweet));
+  }
+);
+
+
+
+
+
+module.exports = router;
